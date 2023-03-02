a8a02f76b556cda53b70843f58f03d116e5c3b74
Junior <juniormach96@gmail.com>
2023-03-01 22:54:40 -0300

    add company model

---
+++ api/models/company.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class Empresa(BaseModel):
+    nome: str
+    endereco: str
+    cpfcnpj: str
+    telefone: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "nome": "Empresa XYZ LTDA",
+                "endereco": "Rua A, 123",
+                "cpfcnpj": "12.345.678/0001-90",
+                "telefone": "(11) 1234-5678"
+            }
+        }
